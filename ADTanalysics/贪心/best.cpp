d33ca212f06512fb6c040186974a4a57e78a6e35
wenqing <wqzHappy@126.com>
2017-06-20 22:15:54 +0800

    为了解决0-1背包问题，设计算法

--- ADTanalysics/贪心/best.cpp
+++ ADTanalysics/贪心/best.cpp
@@ -7,7 +7,14 @@
 #include <cstring>
 
 using namespace std;
+#define Size = 10
 
+typedef struct node{
+    float avervalue;
+    float ww;
+    float vv;
+    float flag;
+}Avervalue;
 
 //贪心算法的活动安排问题实现
 int greedySelector(int s[],int f[],bool a[]){
@@ -34,6 +41,43 @@ int greedySelector(int s[],int f[],bool a[]){
     }
 
     return count;
+}
+
+//用贪心算法来解决0-1背包和背包问题
+float knapsack(float c,float w[],float v[],float x[]){// w：重量 v:价值 x:0 or 1 c:最大允许装的重量
+
+    int n = size;
+    float []d = new float[n];//d存放的是物品的单位价值
+    float swap;
+    for(int i = 0; i < n; i++){
+        d[i] = v[i] / w[i];
+    }
+
+    //将单位价值按数值从大到小排列，保证贪心选择,冒泡排序
+    for(int i = 0; i < n;i++){
+        for(int j = i+1; j < n; j++){
+            if(d[i] < d[j]){
+
+                    swap = d[i];
+                    d[i] = d[j];
+                    d[j] = swap;
+            }
+        }
+    }
+
+    int i;
+    float opt = 0;
+    //这里有个问题，就是排序后怎么知道对应的那个下X和W，所以应该定义一个结构体，
+    for(i = 0; i< n; i++)
+        x[i] = 0;
+
+
+    for(i = 0; i< n;i++){
+        if()
+    }
+
+
+
 }
 
 int main(){
