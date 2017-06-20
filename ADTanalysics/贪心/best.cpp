5775e7488e478283fac7f501468e6c05c6b2f02f
wenqing <wqzHappy@126.com>
2017-06-21 00:44:24 +0800

    have achieved a 0-1 背包问题 by greedy function

--- ADTanalysics/贪心/best.cpp
+++ ADTanalysics/贪心/best.cpp
@@ -7,13 +7,13 @@
 #include <cstring>
 
 using namespace std;
-#define Size = 10
+#define Size  10
 
 typedef struct node{
-    float avervalue;
-    float ww;
-    float vv;
-    float flag;
+    float avervalue;//单位价值
+    float ww;//重量
+    float vv;//价值
+    float flag;//位置
 }Avervalue;
 
 //贪心算法的活动安排问题实现
@@ -46,17 +46,22 @@ int greedySelector(int s[],int f[],bool a[]){
 //用贪心算法来解决0-1背包和背包问题
 float knapsack(float c,float w[],float v[],float x[]){// w：重量 v:价值 x:0 or 1 c:最大允许装的重量
 
-    int n = size;
-    float []d = new float[n];//d存放的是物品的单位价值
-    float swap;
+    int n = Size;
+    Avervalue *d = new Avervalue[n];//d存放的是物品的单位价值
+    Avervalue swap;
+
     for(int i = 0; i < n; i++){
-        d[i] = v[i] / w[i];
+        d[i].avervalue = v[i] / w[i];
+        d[i].flag = i;
+        d[i].vv = v[i];
+        d[i].ww = w[i];
+
     }
 
     //将单位价值按数值从大到小排列，保证贪心选择,冒泡排序
     for(int i = 0; i < n;i++){
         for(int j = i+1; j < n; j++){
-            if(d[i] < d[j]){
+            if(d[i].avervalue < d[j].avervalue){
 
                     swap = d[i];
                     d[i] = d[j];
@@ -66,16 +71,30 @@ float knapsack(float c,float w[],float v[],float x[]){// w：重量 v:价值 x:0 or 1
     }
 
     int i;
-    float opt = 0;
+    float opt = 0;//opt 为定义的价值总量
+
     //这里有个问题，就是排序后怎么知道对应的那个下X和W，所以应该定义一个结构体，
     for(i = 0; i< n; i++)
         x[i] = 0;
 
+    for( i = 0; i < n;i++){
+        if(d[i].ww > c)
+            break;
 
-    for(i = 0; i< n;i++){
-        if()
+        x[(int)d[i].flag] = 1;//这里如果不强制转换会出错，注意！！！
+        opt = d[i].vv;
+        c -= d[i].ww;
+
     }
 
+    //判断是否取尽所有物品
+    if(i < n){
+        x[(int)d[i].flag] = c/d[i].ww;
+        opt += x[(int)d[i].flag] * d[i].vv;
+    }
+
+
+    return opt;
 
 
 }
