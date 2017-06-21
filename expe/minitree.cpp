8967ee797f0f76b4d72f68a015899a851322ff0a
wenqing <wqzHappy@126.com>
2017-06-21 20:34:40 +0800

    m增加最小生成树的算法实现

---
+++ expe/minitree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <ostream>
+
+using namespace std;
+
+#define MAX_N 10
+
+//实现最小生成树的结构定义
+typedef struct {
+    int n;
+    int arcw[MAX_N][MAX_N];
+}Graph;
+
+
+typedef struct {
+    double lowcost;
+    int vex;
+}CostTp;
+
+
+
+void crtGraph(Graph &G){
+
+
+
+
+}
+
+void prim(Graph &G,int k){
+
+
+
+
+}
+
+int main(){
+    return 0;
+
+
+}
