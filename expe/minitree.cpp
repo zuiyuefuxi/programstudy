2b4694f650a8cee369bd687e62278054852b8ead
wenqing <wqzHappy@126.com>
2017-06-22 17:20:43 +0800

    实现了最小生成树算法，并且附带测试数据

--- expe/minitree.cpp
+++ expe/minitree.cpp
@@ -11,6 +11,7 @@ using namespace std;
 //实现最小生成树的结构定义
 typedef struct {
     int n;
+    int e;
     int arcw[MAX_N][MAX_N];
 }Graph;
 
@@ -23,20 +24,128 @@ typedef struct {
 
 
 void crtGraph(Graph &G){
+    int i,j,k;
+    int w;
 
+    ifstream fin("input.txt");
+    //ofstream fout("out.txt");
+    if(!fin ){
+        return ;
+    }
 
+    fin >> G.n >> G.e;
 
+    //初始化全部为-1
+    for(i = 0; i < G.n;i++){
+        for(j = 0; j < G.n; j++){
+            G.arcw[i][j] = 333;
+        }
+    }
+
+    //存入相应的权值
+    for(i = 0; i < G.e; i++){
+        fin >> j >> k >> w;
+        G.arcw[j][k] = G.arcw[k][j] = w;
+    }
+
+    fin.close();
+
+   // fout << G.n << " " << G.e << endl;
+
+/*
+    //检验是否写入正确
+    for(i = 0; i < G.n; i++){
+        for(j = 0; j< G.n; j++){
+            fout << G.arcw[i][j] << "  ";
+        }
+
+        fout << endl;
+    }
+
+    fout.close();
+*/
 
 }
 
 void prim(Graph &G,int k){
 
+    CostTp *closedge = new CostTp[G.n];
+    int *saveroad = new int[G.n];
+    int si = 0;
+    int maxload = 0;
 
+    ofstream fout("output.txt");
+    if(!fout)
+        return;
 
+    //初始化closedge
+    for(int i = 0; i < G.n; i++){
+        closedge[i].vex = k;
+        closedge[i].lowcost = G.arcw[i][k];
+    }
+
+    closedge[k].lowcost = 0;
+
+    cout << "依次遍历的结点如下："<< endl;
+    cout <<k;
+    saveroad[si++] = k;
+    int m;
+    int i,j;
+
+    for(m = 0; m < G.n - 1; m++){
+
+        for( i = 0; i< G.n; i++){
+            if(closedge[i].lowcost > 0)
+                break;
+        }
+
+        for(j = i + 1; j < G.n; j++){
+            if(closedge[j].lowcost > 0 && closedge[j].lowcost < closedge[i].lowcost)
+                i = j;
+        }
+
+        cout << "," << i;//输出i号定点
+        saveroad[si++] = i;
+
+        closedge[i].lowcost = 0;
+
+        for(j = 0; j< G.n; j++){
+            if(closedge[j].lowcost > 0 && closedge[j].lowcost > G.arcw[j][i]){
+                closedge[j].lowcost = G.arcw[j][i];
+                closedge[j].vex = i;
+            }
+        }
+    }
+
+    //for循环结束
+    cout << endl;
+    cout << "生成树的五条边以及权重分别是：" << endl;
+    fout << "生成树的五条边分别是：" << endl;
+    for(i = 0; i< G.n;i++){
+        if(i != k){
+            cout << "(" << i << ","<< closedge[i].vex << ")";
+            maxload +=  G.arcw[i][closedge[i].vex];
+            fout << "(" << i << ","<< closedge[i].vex << ")";
+            cout << "--" << G.arcw[i][closedge[i].vex] << endl;
+
+        }
+    }
+
+    fout << endl;
+    cout << "最小生成树权重和为： " << maxload << endl;
+    fout << "最小生成树权重和为： " << maxload << endl;
+    fout.close();
+
+    delete []closedge;
 
 }
 
 int main(){
+
+    Graph G;
+    crtGraph(G);
+    prim(G,0);
+
     return 0;
 
 
