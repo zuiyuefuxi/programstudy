49bbe228a9e71dc9deb556f1cb13d7852afc2baa
wenqing <wqzHappy@126.com>
2017-06-28 22:02:32 +0800

    初试Python

---
+++ androiddevelop/Pythonstudy/hello.py
@@ -0,0 +1,39 @@
+import random
+
+a = random.randint(1,10)
+print(a)
+'''
+if a > 10:
+    print("xxxx")
+    print("a = 12")
+
+if a > 11:
+    print("mmmm")
+elif a == 12:
+    print("zzzz")
+else:
+    print("end")
+
+b = [1,2,3,4,5,6]
+
+if 7  in b:
+    print("7 在列表中")
+else:
+    print("7 不在列表中")
+
+print("xxxx")
+'''
+
+
+# input()猜字游戏
+number = input('请输入数字：')
+
+if number.isdigit()
+if number == a:
+    print("猜对了")
+elif number > a:
+    print('大了')
+elif number < a:
+    print('小了')
+
+print('这个数其实是：',a)
