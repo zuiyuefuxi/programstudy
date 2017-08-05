d8cc10855a47f5a88def78067402d789cad6657d
wenqing <wqzHappy@126.com>
2017-08-05 14:40:23 +0800

    增加迭代和列表生成器的功能

---
+++ pythonstudy/spider.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  5 10:36:30 2017
+
+@author: DELL
+"""
+import random
+
+# #迭代
+#
+#d =  {'a':1,'b':2,'c':3}
+#for key in d:
+#    print(key)
+#    
+#'''
+#默认情况下，dict迭代的是key
+#如果要迭代value，可以用for value in d.values()
+#如果要同时迭代key和value，可以用for k, v in d.items()
+#如何判断一个对象是可迭代对象呢？方法是通过collections模块的Iterable类型判断
+#'''
+#print('迭代变量')
+#for value in d.values():
+#    print(value)
+#    
+# # 字符串也可以迭代
+#for i in 'abc':
+#    print(i)
+#    
+# # Python内置的enumerate函数可以把一个list变成索引-元素对，这样就可以在for循环中同时迭代索引和元素本身
+#print('使用下标迭代')
+#for i,value in enumerate(['a','b','d']):
+#    print(i,value)
+#
+# # 同时引用两个变量;
+#print('同时引用两个变量')
+#for x,y in [(1,1),(2,2),(4,4)]:
+#    print(x,y) 
+#    
+    
+
+# 列表生成式
+l(range(1,11)
+print(l)
